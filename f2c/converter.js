51e8a8a0bdd52637f7b41c665d8c37a1cd3b7ee3
Gareth <gerty3@gmail.com>
2012-08-22 17:45:13 +1200

    Added functionality for converting weight (lb, oz) as well as temperature to metric.

--- f2c/converter.js
+++ f2c/converter.js
@@ -8,8 +8,8 @@ function walk(node) {
     switch (node.nodeType) {
         case 1: // Element
             for (child = node.firstChild;
-                child;
-                child = child.nextSibling) {
+            child;
+            child = child.nextSibling) {
                 walk(child);
             }
             break;
@@ -24,29 +24,41 @@ function walk(node) {
 
 function tempCheck(node) {
     
-    var reTemp = /\b((\d{2,3})\s*\-*(to)*\s*)?(\d{2,3})'*\s*\u00B0*\u00BA*\s*f\b/gi;
+    // Regex returns an array containing:
+    // [0] The entire match.
+    // [1] Weight value, null if temp.
+    // [2] Weight units, null if temp.
+    // [3] First temp if a range exists, null if weight or no range.
+    // [4] Second temp if a range exists, or the lone temp. null if weight.
+    var re = /\b(\d{1,4})\s*(lbs?|#|ozs?)|(?:\b(?:(\d{2,3})\s*(?:-|to)\s*)?(\d{2,3})'?\s*\u00B0?\u00BA?\s*f)\b/gi;
+
     var nodeText = node.nodeValue;
     var first = 0;
     var newFragment = document.createDocumentFragment();
 
     // Check for Fahrenheit temperature match.
-    for (match = reTemp.exec(nodeText); match != null; match = reTemp.exec(nodeText)) {
-                
+    for (match = re.exec(nodeText); match != null; match = re.exec(nodeText)) {
+
+        console.log(match);
+        
+        
         // Attach the text leading up to the first match.
-        var leadUp = nodeText.substring(first, reTemp.lastIndex - match[0].length);
+        var leadUp = nodeText.substring(first, re.lastIndex - match[0].length);
         var leadUpNode = document.createTextNode(leadUp);
         newFragment.appendChild(leadUpNode);
-                
-        // Convert the matched F temp to C and create a new span for it.
+
+        var spanText = convert(match);
+
+        // Create a new span for the converted value.
         var newSpan = document.createElement('span');
-        newSpan.setAttribute('class', 'f2c_temp');
+        newSpan.setAttribute('class', 'f2c');
         newSpan.setAttribute('title', match[0]);
 
-        var tempInC = document.createTextNode(convertTemp(match));
+        var tempInC = document.createTextNode(spanText);
         newSpan.appendChild(tempInC);
         newFragment.appendChild(newSpan);
                     
-        first = reTemp.lastIndex;
+        first = re.lastIndex;
     }
             
     // Alter the original text node to be the remaining text
@@ -58,20 +70,61 @@ function tempCheck(node) {
     
 }
 
-function convertTemp(match) {
-
+function convert(match) {
+    
     var replacement = '';
-
-    var num1 = parseFloat(match[2]);
-    if (!isNaN(num1)) {
-        replacement += ((num1 - 32) * 5/9).toFixed(2) + ' - ';
-    }
+    
+    // Determine if the match is for a weight or temperature.
+    if (match[2] != null) {
             
-    var num2 = parseFloat(match[4]);
-    if (!isNaN(num2)) {
-        replacement += ((num2 - 32) * 5/9).toFixed(2) + ' \u00B0C';
+        // Weight conversion.
+        replacement = convertWeight(match);
+        
+    } else if (match[4] != null) {
+            
+        // Temperature conversion.
+        var num1 = parseFloat(match[3]);
+        if (!isNaN(num1)) {
+            replacement += convertTemp(num1) + ' - ';
+        }
+            
+        var num2 = parseFloat(match[4]);
+        if (!isNaN(num2)) {
+            replacement += convertTemp(num2) + ' \u00B0C';
+        }
+            
     }
-
+        
     return replacement;
     
 }
+
+
+function convertTemp(temp) {
+
+    return ((temp - 32) * 5/9).toFixed(2);
+    
+}
+
+
+function convertWeight(match) {
+    
+    var replacement = '';
+    var value = parseFloat(match[1]);
+    
+    if (match[2].search(/lb|#/) != -1) {
+            
+        // Pounds.
+        replacement = (value * 0.453592).toFixed(2) + 'kg';
+            
+    } else {
+            
+        // Ounces.
+        replacement = (value * 28.3495).toFixed(2) + 'g';
+            
+    }
+    
+    return replacement;
+    
+}
+
